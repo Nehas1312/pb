07d3aa067bc06bf08adf8eebec080bb514b1fea1
Nehas1312 <nehaskandiraju@gmail.com>
2022-05-23 17:26:42 +0530

    phonebook db st3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const app = express();
 app.use(express.json())
 app.use(express.static('build'))
-const Person =require("./models/persons")
+const Contacts =require("./models/persons")
 
 morgan.token('type', (request,response) => 
 {
@@ -15,44 +15,48 @@ morgan.token('type', (request,response) =>
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abraham",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary jane",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abraham",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary jane",
+//     number: "39-23-6423122",
+//   },
+// ];
 
 const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
+  const maxId = Contacts.length > 0
+    ? Math.max(...Contacts.map(n => n.id))
     : 0
   return maxId + 1
 }
 
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+
+  Contacts.find({}).then(Contacts =>
+    {
+        response.json(Contacts)
+    })
 });
 
 app.get("/info", (request, response) => {
   response.send(`<div>
-                    <p>The Phonebook has ${persons.length} contacts</p>
+                    <p>The Phonebook has ${Contacts.length} contacts</p>
                     <p> ${new Date()} </p>
                  </div>   
                  `);
@@ -61,7 +65,7 @@ app.get("/info", (request, response) => {
 
 app.get('/api/persons/:id',(request,response)=>{
   const id = Number(request.params.id)
-  const person = persons.find(person => person.id == id )
+  const person = Contacts.find(person => person.id == id ).catch(error=>next(error))
   
   if (person){
     response.json(person)
@@ -71,51 +75,102 @@ app.get('/api/persons/:id',(request,response)=>{
   } 
 })
 // app.get('/api/persons/:id', (request, response) => {
-//   Person.findById(request.params.id).then(person => {
+//   Contacts.findById(request.params.id).then(person => {
 //     response.json(person)
 //   })
 // })
 
 
-app.post('/api/persons',(request,response)=>{
+// app.post('/api/persons',(request,response)=>{
+//   const body = request.body
+//   console.log(request.body)
+  
+//   if (Contacts.findIndex((person) => person.name === body.name) !== -1) {
+//     const person = Contacts( {
+//       name : body.name,
+//       number:body.number,
+//       date: new Date(),
+//       id:body.id,
+//     })
+//   }
+  
+//    else if (!body.name || !body.number){
+//     return  response.status(404).json({
+//     error :'name or number is missing'
+//   })
+//   }
+
+//   const person =new Contacts( {
+//     name : body.name,
+//     number:body.number,
+//     date: new Date(),
+//     id:generateId(),
+//   })
+  
+//   Contacts = Contacts.concat(person)
+
+//   morgan.token()
+//   Contacts.save().then(savedContacts=>{
+//     response.json(savedContacts)
+//   })
+// })
+app.post('/api/persons',(request,response) =>
+{
+    const body = request.body
+
+    if(body.name === undefined)
+    {return response.status(400).json({error : 'content missing'})}
+    
+    const person = new Contacts(
+        {
+            name : body.name,
+            number : body.number,
+        }
+    )
+
+    person.save().then(savedContacts =>
+        {
+            response.json(savedContacts)
+        })
+}) 
+
+app.put('/api/persons/:objectId', (request, response, next) => {
   const body = request.body
-  console.log(request.body)
-  
-  if (persons.findIndex((person) => person.name === body.name) !== -1) {
-    return response.status(404).json({
-      error : 'name must be unique'
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Contacts.findByIdAndUpdate(request.params.objectId, person, { new: true })
+    .then(updatedContact=> {
+      response.json(updatedContact)
     })
-  }
-  
-   else if (!body.name || !body.number){
-    return  response.status(404).json({
-    error :'name or number is missing'
-  })
-  }
-
-  const person =new Person( {
-    name : body.name,
-    number:body.number,
-    date: new Date(),
-    id:generateId(),
-  })
-  
-  persons = persons.concat(person)
-
-  morgan.token()
-  person.save().then(savedPerson=>{
-    response.json(savedPerson)
-  })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id',(request,response)=>{
-  const id = Number(request.params.id)
-  const person = persons.filter(person => person.id !== id)
-  persons=person
-  persons.save()
+app.delete('/api/persons/:objectId',(request,response)=>{
+//  const idToDelete =(request.params.objectId)
+Contacts.findByIdAndRemove(request.params.objectId).then(result => {
   response.status(204).end()
+})
+.catch(error => next(error))
+
+  console.log(Contacts)
   console.log(`Deleted sucessfully`)
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
